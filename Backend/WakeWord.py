4daa976303453de1ec0c262570dd09076e921516
Ankit863326 <rishisinghri226@gmail.com>
2026-05-05 00:34:05 +0530

    create new file wakeword.py/Backend

---
+++ Backend/WakeWord.py
@@ -0,0 +1,19 @@
+import speech_recognition as sr
+
+WAKE_WORD = "swayam"
+
+def wait_for_wake_word():
+    r = sr.Recognizer()
+    print("😴 Sleeping... say 'Swayam' to wake me up")
+    while True:
+        with sr.Microphone() as source:
+            r.adjust_for_ambient_noise(source, duration=0.3)
+            try:
+                audio = r.listen(source, timeout=4, phrase_time_limit=3)
+                text = r.recognize_google(audio).lower()
+                print(f"[WakeWord] Heard: {text}")
+                if WAKE_WORD in text:
+                    print("✅ Swayam activated!")
+                    return
+            except:
+                pass
